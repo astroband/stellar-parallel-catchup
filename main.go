4bf570571f4942fa5e384a1acb75589f432ee9e2
Viktor Sokolov <gzigzigzeo@gmail.com>
2019-07-29 14:07:21 +0300

    Precalculations

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/astroband/stellar-parallel-catchup/config"
 	"github.com/astroband/stellar-parallel-catchup/db"
 	"github.com/gammazero/workerpool"
@@ -9,11 +12,12 @@ import (
 var pool = workerpool.New(*config.Concurrency)
 
 func main() {
+	log.Println("stellar-parallel-catchup ", config.Version)
+
 	gaps := db.GetGaps()
 
 	for index, gap := range gaps {
-
-		// fmt.Println(index, gap.Chunks, gap.Tail)
+		fmt.Println(index, gap.Chunks, gap.Tail)
 	}
 
 	pool.StopWait()
